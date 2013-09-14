44bf8e75815d6b361acf688dee4bfc73a21b2495
Manuel Orihuela <admin@saicasl.eu>
2013-09-14 14:07:29 +0200

    Proyecto 1 GOOGLEMAPS

--- Project1/unit1_php.js
+++
@@ -1,4 +0,0 @@
-jQuery('#MPage1').bind('pageinit',function(event){
- 	jQuery('#MButton1').bind('click',MButton1Click);
-
-});
--- Project2/unit1.js
+++
@@ -1,5 +0,0 @@
-// This is where you can place your Javascript code
-
-function MButton1Click(event) {
-    alert('asdfads');
-}
